9efac4fc423323434a35aad750fdabe788bc22f3
Davi Lorena <davilorena2016@gmail.com>
2025-07-28 23:59:36 -0300

    feat: adiciona tratamento de excecoes para zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 import { AppError } from "@/utils/AppError"
+import { ZodError } from "zod"
 
 export function errorHandling(
   error: any,
@@ -11,6 +12,13 @@ export function errorHandling(
   if(error instanceof AppError) {
     return res.status(error.statusCode).json({
       message: error.message,
+    });
+  }
+
+  if(error instanceof ZodError) {
+    return res.status(400).json({
+      message: "Validation error",
+      issues: error.format()
     });
   }
 
