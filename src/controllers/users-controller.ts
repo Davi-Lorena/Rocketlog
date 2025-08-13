f79507a99f38ea407f01c19063becd6f8c94fbd3
Davi Lorena <davilorena2016@gmail.com>
2025-08-13 20:45:44 -0300

    feat: adiciona validacao do corpo da requisicao

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express"
+import { z } from "zod"
 
 class UsersController {
     create(req: Request, res: Response)  {
+        const bodySchema = z.object({
+            name: z.string().trim().min(2),
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const { name, email, password } = bodySchema.parse(req.body)
+        
+
         return res.status(201).json({
-            message: "User created"
-        });
+            message: "OK", name, email, password
+        })
+
     }
 }
 
