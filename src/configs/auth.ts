0a54664ff1f3c26a18740548fb0e332700ea9c7d
Davi Lorena <davilorena2016@gmail.com>
2025-08-13 21:58:12 -0300

    feat: valida variaveis de ambiente via zod

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,6 +1,8 @@
+import { env } from "../env"
+
 export const authConfig = {
     jwt: {
-        secret: process.env.JWT_SECRET,
+        secret: env.JWT_SECRET,
         // Em uma situação profissional real, essa variável de ambiente acima poderia ser um hash personalizado para dar mais segurança.
         expiresIn: "1d"
     }
