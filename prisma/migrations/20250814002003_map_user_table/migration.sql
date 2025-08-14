f72e232009ee842a8eb9f232dc24f6998aef6a22
Davi Lorena <davilorena2016@gmail.com>
2025-08-13 21:22:03 -0300

    feat: adiciona usuario no banco

---
+++ prisma/migrations/20250814002003_map_user_table/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "deliveries" DROP CONSTRAINT "deliveries_user_id_fkey";
+
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'customer',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
 import { hash } from "bcrypt"
 import { z } from "zod"
 
@@ -9,14 +11,34 @@ class UsersController {
             email: z.string().email(),
             password: z.string().min(6),
         })
-
+        
         const { name, email, password } = bodySchema.parse(req.body)
+
+        const userWithSameEmail = await prisma.user.findFirst({
+            where: {
+                 email 
+            }
+        })
+
+        if(userWithSameEmail) {
+            throw new AppError("User with this email already exists")
+        }
         
 const hashedPassword = await hash(password, 8)
 
-        return res.status(201).json({
-            message: "OK", name, email, password, hashedPassword
-        })
+const user = await prisma.user.create({ 
+    data: {
+        name,
+        email,
+        password: hashedPassword
+        // Podemos notar que nessas linhas de código acima utilizamos a mesma lógica do "where: { email }", tendo em vista que, diferentemente da senha, para name e email temos implicitamente um "name: name"
+    }
+})
+
+// Na linha abaixo eu remove a senha do usuário e utilizo o spread para capturar todos os outros parâmetros para exibição
+const {password: _, ...userWithoutPassword} = user
+
+        return res.status(201).json(userWithoutPassword)
 
     }
 }
