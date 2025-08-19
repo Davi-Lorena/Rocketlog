2a6fbb7b38b7e55d842fe5c4b0d2e2c7b41c14e4
Davi Lorena <davilorena2016@gmail.com>
2025-08-19 09:06:08 -0300

    feat: adiciona metodo para cadastrar um delivery

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from 'express'
+import { prisma } from '@/database/prisma'
+import { z } from 'zod'
 
 class DeliveriesController {
     async create(req: Request, res: Response) {
         
-        return res.json({ message: "OK "})
+const bodySchema = z.object({
+    user_id: z.string().uuid(),
+    description: z.string(),
+})
+
+const { user_id, description } = bodySchema.parse(req.body)
+
+await prisma.delivery.create({
+    data: {
+        userId: user_id,
+        description
+    }
+})
+
+        return res.status(201).json()
     }
 }
 
