2981ffe310b59b60d168b9f3f2d7341db087822e
Davi Lorena <davilorena2016@gmail.com>
2025-08-19 09:22:44 -0300

    feat: adiciona metodo para atualizar status de um delivery

---
+++ src/controllers/deliveries-status-controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { prisma } from '@/database/prisma'
+import { z } from 'zod'
+
+class DeliveriesStatusController {
+async update(req: Request, res: Response) {
+
+    // id está nos parâmetros, status está no corpo
+    const paramsSchema = z.object({
+        id: z.string().uuid(),
+    })
+
+const bodySchema = z.object({
+    status: z.enum(["processing", "shipped", "delivered"]),
+})
+
+const { id } = paramsSchema.parse(req.params)
+const { status } = bodySchema.parse(req.body)
+
+await prisma.delivery.update({
+   data: {
+    status,
+   },
+   where: {
+    id,
+   },
+
+})
+
+return res.json()
+}
+}
+
+export { DeliveriesStatusController }
