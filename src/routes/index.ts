bf5eef5d75c196780fd4a7911b2bdf454c8df986
Davi Lorena <davilorena2016@gmail.com>
2025-08-20 20:45:56 -0300

    feat: adiciona arquivos e configuracoes iniciais para logs de delivery

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express"
 import { usersRouter } from "./users-routes"
 import { sessionsRoutes } from "./sessions-routes"
 import { deliveriesRoutes } from "./deliveries-routes"
+import { deliveryLogsRoutes } from "./delivery-logs-routes"
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/sessions", sessionsRoutes)
 routes.use("/deliveries", deliveriesRoutes)
+routes.use("/delivery-logs", deliveryLogsRoutes)
 
 
 export { routes }
