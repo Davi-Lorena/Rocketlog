c0a87b9bb555df78ef08c861dcf7c83d578aad1c
Davi Lorena <davilorena2016@gmail.com>
2025-08-20 22:03:36 -0300

    feat: adiciona teste para cadastro de usuario

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,28 @@
+import request from "supertest"
+import { app } from "@/app"
+import { prisma } from "@/database/prisma"
+
 describe("UsersController", () => {
+    let user_id: string
+
+afterAll(async () => {
+    await prisma.user.delete({
+        where: { id: user_id}
+    })
+})
+
+    it("should create a new user Succesfully", async () => {
+    const response = await request(app).post("/users").send({
+        name: "test user",
+        email: "testuser@example.com",
+        password: "password123"
+    })
     
-    it("test", () => {
-     console.log("Passou aqui")
+expect(response.status).toBe(201)
+expect(response.body).toHaveProperty("id")
+expect(response.body.name).toBe("test user")
+
+user_id = response.body.id
 
  })
 });
